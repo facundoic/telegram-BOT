7a1d0c2cfd3b6e7f5e21ebf735acdb660286423b
facundoic <facundoceballos1994@hotmail.com>
2021-07-21 12:02:16 -0300

    first changes

---
+++ src/bot.py
@@ -0,0 +1,11 @@
+import logging
+from telegram.ext import Updater
+
+# Configurate logging
+logging.basicConfig(
+    Level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(nessage)s"
+)
+logger = logging.getLogger()
+# Request TOKEN
+
+# 1905184735:AAEKpC3oB8NcWxSP6RQTusxjirmrWS4jlWE
