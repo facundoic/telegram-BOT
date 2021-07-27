779d1482ae2e040bd7385d80d73ff3c88ef14783
facundoic <facundoceballos1994@hotmail.com>
2021-07-27 09:54:07 -0300

    new implements in the bot

--- src/bot.py
+++ src/bot.py
@@ -1,11 +1,21 @@
 import logging
-from telegram.ext import Updater
+import telegram
+from dotenv import load_dotenv
+import os
 
-# Configurate logging
-logging.basicConfig(
-    Level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(nessage)s"
-)
-logger = logging.getLogger()
-# Request TOKEN
+load_dotenv()
+token = os.getenv("TOKEN")
 
-# 1905184735:AAEKpC3oB8NcWxSP6RQTusxjirmrWS4jlWE
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+bot = telegram.Bot(token=token)
+print('------------------------------------------WHO I AM')
+print(bot.get_me())
+
+print('---------------------------------------WHO SENDs ME ')
+updates = bot.get_updates()
+print(updates[0])
+
+bot.send_message(text='Hola Facundo!', chat_id=751034701)
